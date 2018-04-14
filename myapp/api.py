c1107bbf4175f45c0234afb0fe60e65d6039aace
shahriar1628 <shahriarcsedu@gmail.com>
2018-04-15 01:22:41 +0600

    forieghn key add

--- myapp/api.py
+++ myapp/api.py
@@ -11,6 +11,7 @@ class PersonResource(ModelResource):
         queryset = Person.objects.all()
         resource_name= 'person'
         authorization = Authorization()
+        fields = ['id', 'firstName', 'lastName']
 
 class SectionResource(ModelResource):
     class Meta:
@@ -19,11 +20,12 @@ class SectionResource(ModelResource):
         authorization = Authorization()
 
 class StudentResource(ModelResource):
-    person= fields.OneToOneField(PersonResource,'person')
+    person= fields.OneToOneField(PersonResource,'person',full=True)
+    #person_id = fields.IntegerField(attribute='person_id', null=True)
     section= fields.ForeignKey(SectionResource, 'section')
 
     class Meta:
-        queryset = student.objects.all()
+        queryset = student.objects.all().select_related("person")
         resource_name= 'pupil'
         authorization = Authorization()
 
